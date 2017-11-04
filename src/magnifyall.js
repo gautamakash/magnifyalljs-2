9ae65627bb4c547ef35651dd36bae646af8c1cb1
akash gautam <akashgautam007@gmail.com>
2017-11-05 00:09:03 +1100

    fixed multiple import

--- src/magnifyall.js
+++ src/magnifyall.js
@@ -11,7 +11,7 @@ var magnifyall = {
  */
 magnifyall.System = function(_settings){
     _settings = _settings || {};
-    
+
     // System Name
     this.name = _settings.name || "default";
     // Check if system is already initialize then return the System
@@ -27,7 +27,7 @@ magnifyall.System = function(_settings){
     // Setting debug flag
     this.debug = (_settings && _settings.debug)?_settings.debug:false;
     // BeanFactory Configuration
-    
+
 
 
     /*
@@ -51,7 +51,7 @@ magnifyall.System = function(_settings){
     // System Cache
     var _cache = {};
 
-    
+
     // generate GUID
     /**
      * Generate UID
@@ -68,7 +68,7 @@ magnifyall.System = function(_settings){
             s4() + '-' + s4() + s4() + s4();
     };
     // Register Object to Pool
-    
+
     /**
      * Register System Generated Object:- Could be use to register diffrent System object
      * @public
@@ -79,7 +79,7 @@ magnifyall.System = function(_settings){
             _uids[_obj.__UID] = _obj;
         }
     };
-    // get Object from pool    
+    // get Object from pool
     /**
      * Get object by UID if generated or register by current System
      * @public
@@ -90,7 +90,7 @@ magnifyall.System = function(_settings){
         return _uids[_uid];
     };
     // Import Class
-     
+
     /**
      * Import Class in system from System source path
      * @public
@@ -109,7 +109,7 @@ magnifyall.System = function(_settings){
         }
         return this;
     };
-    
+
     // Append Bean Config
     /**
      * Append Bean Configutation for package defination (cann't override if already define).
@@ -141,7 +141,7 @@ magnifyall.System = function(_settings){
                     _callback(_beans[_dataId]);
                     return;
                 }
-                
+
                 if(_beanConfig && _beanConfig.fetchService){
                     _beanConfig.fetchService(_query, function(_dataString){
                         _data = JSON.parse(_dataString);
@@ -192,7 +192,7 @@ magnifyall.System = function(_settings){
     };
 
     //Template Processor
-    
+
     /*
      * Template Processor
      */
@@ -219,13 +219,13 @@ magnifyall.System = function(_settings){
             this.rootElement = _ele;
             if(!this.elements){
                 this.elements = {};
-            } 
+            }
             if(!isAppend){
                 while (this.rootElement.firstChild) {
                     this.rootElement.removeChild(this.rootElement.firstChild);
                 }
             }
-            for(var _templateIndex = 0; _templateIndex<__template.length; _templateIndex++){           
+            for(var _templateIndex = 0; _templateIndex<__template.length; _templateIndex++){
                 var _childTemplate = __template[_templateIndex];
                 var _childElement = this.populateElement(this.rootElement,_childTemplate, __data);
                 if(_childElement){
@@ -235,7 +235,7 @@ magnifyall.System = function(_settings){
         }
         this.populateElement = function(_ele, _template, _data){
             var _retObj = '';
-            for(var _eleName in _template){                
+            for(var _eleName in _template){
                 if(_eleName === 'render'){
                     if(_data.__render){
                         var _isRenderAppend = false;
@@ -287,7 +287,7 @@ magnifyall.System = function(_settings){
                             }
                             _retObj.setAttribute("style", _styleString);
                             //console.log(_styleString);
-                        }else if(_propName.indexOf('on') == -1){                        
+                        }else if(_propName.indexOf('on') == -1){
                             _retObj.setAttribute(_propName, this.getBindInfo(_template[_eleName].prop[_propName],_data).value);
                         }else if(_propName.indexOf('on') == 0){
                             // bind methods
@@ -351,7 +351,7 @@ magnifyall.System = function(_settings){
                         _value = _data['@root'][_retObj.type](_value);
                     }
                     _template = _template.replace(_directive, _value);
-                }            
+                }
             }
             _retObj.value = _template;
             return _retObj;
@@ -362,7 +362,7 @@ magnifyall.System = function(_settings){
             }
             if(_nameSpace.indexOf('.') != -1){
                 var _parentNameSpace = _nameSpace.split('.')[0];
-                if(_data[_parentNameSpace]){                
+                if(_data[_parentNameSpace]){
                     return this.getDataValue(_data[_parentNameSpace], _nameSpace.substring(_parentNameSpace.length+1,_nameSpace.length));
                 }
             }
@@ -376,7 +376,7 @@ magnifyall.System = function(_settings){
         }
         return new TemplateProcessor({});
     }
-    
+
     /*
     Private Methods
     */
@@ -389,7 +389,7 @@ magnifyall.System = function(_settings){
         }
         return _retObj;
     };
-    /* 
+    /*
     return String value from response take file path, should cache, system
     */
     var _getFile = function(_filePath, uncached, _callback){
@@ -406,7 +406,7 @@ magnifyall.System = function(_settings){
                 }
                 if (this.readyState == 4 && this.status == 200) {
                     _callback(_returnString);
-                }else if(this.readyState == 4){                    
+                }else if(this.readyState == 4){
                     _callback(false);
                 }
             };
@@ -426,11 +426,11 @@ magnifyall.System = function(_settings){
                 _baseObj = _baseObj[_fileArr[_fileArrI]];
             }else{
                 _className = _fileArr[_fileArrI];
-            }            
+            }
         }
         var _methodRegEx = new RegExp("([^a-zA-Z_.]|^)"+_className+"[ ]{0,}=", "g");
         _fileString = _fileString.replace(_methodRegEx, "\n"+_package+" =");
-        
+
         return _fileString;
     };
 
@@ -450,7 +450,7 @@ magnifyall.System = function(_settings){
                 _aspectString += ' this.__getSystem().registerObject(this); \n';
                 var _aspectRegex = new RegExp("function[ ]{0,}([^{]*){", "");
                 var _functionArr = _fileString.match(_aspectRegex);
-                var _preFunctionString = _fileString.substring(0,_functionArr.index+_functionArr['0'].length);        
+                var _preFunctionString = _fileString.substring(0,_functionArr.index+_functionArr['0'].length);
                 var _postFunctionString = _fileString.substring(_functionArr.index+_functionArr['0'].length, _fileString.length);
                 _fileString = _preFunctionString+" \n"+_aspectString+" \n"+_postFunctionString;
             }catch(e){
@@ -460,7 +460,7 @@ magnifyall.System = function(_settings){
         });
     };
 
-    var _importLoopTrigger = function(){        
+    var _importLoopTrigger = function(){
             if(_importQueue.length>0){
                 _importProcess();
             }else{
@@ -476,7 +476,7 @@ magnifyall.System = function(_settings){
         //var _aopRegEx = new RegExp("(this.|var ( ){0,})[a-zA-Z_0-9.]{0,}( ){0,}=( ){0,}function\([a-zA-Z_0-9 ,]{0,}\)( ){0,}{", "g");
         var _functionsStrArr = _fileString.match(/((this.|var ( ){0,})[a-zA-Z_0-9.]{0,}( ){0,}=( ){0,}){0,}(((function)( ){0,}\([a-zA-Z_0-9 ,]{0,}\)( ){0,}{)|\(( ){0,}[a-zA-Z_0-9.]{0,}( ){0,}\)( ){0,}=>( ){0,}\{)/g);
         if(_functionsStrArr && _functionsStrArr.length && _functionsStrArr.length>1){
-            
+
             for(var _functionStrI = 1; _functionStrI< _functionsStrArr.length; _functionStrI++){
                 var _functionStr = _functionsStrArr[_functionStrI];
                 var _aopConfig = _getAOPDetail(_functionStr, _package);
@@ -491,17 +491,17 @@ magnifyall.System = function(_settings){
                         _fnDefination += "\n";
                         _fnDefination += "magnifyall.__aops['"+_aopKey+"'].before("+_aopData+", arguments);\n";
                     }
-                    
+
                     _fileString = _fnStringArr[0]+_fnDefination+_fnStringArr[1];
                 }
             }
-            
+
         }
         //console.log(_functionsStrArr);
-        
+
         return _fileString;
     };
-    
+
     var _getAOPDetail = function(_fnStr, _package){
         var _aopDetail = false;
         for(var _aopI = 0; _aopI< _aopConfiguration.length; _aopI++){
@@ -536,7 +536,7 @@ magnifyall.System = function(_settings){
                 }
             }
         }
-        
+
         return _aopDetail;
     };
 
@@ -547,7 +547,7 @@ magnifyall.System = function(_settings){
         //console.log('_addDependedImport');
         var _importStrArr = _fileString.match(/this.import( ){0,}\(( ){0,}['"][a-zA-Z0-9.]*['"]( ){0,}\)(( ){0,};){0,}/g);
         if(_importStrArr && _importStrArr.length && _importStrArr.length>0){
-            
+
             for(var _importStrI = 0; _importStrI< _importStrArr.length; _importStrI++){
                 var _importStr = _importStrArr[_importStrI];
                 var _importPackage = _importStr.split(/['"]/)[1];
@@ -556,11 +556,11 @@ magnifyall.System = function(_settings){
                 _importConfig.imports.push(_importPackage);
                 if(!_classList[_importPackage])_importQueue.unshift({package: _importPackage});
             }
-            
+
         }
         return _fileString;
     }
-    
+
     /*
      * Depending import
      */
@@ -569,7 +569,7 @@ magnifyall.System = function(_settings){
         var _isExtendClassIsImported = true;
         var _packageArr = [];
         if(_importStrArr && _importStrArr.length && _importStrArr.length>0){
-            
+
             for(var _importStrI = 0; _importStrI< _importStrArr.length; _importStrI++){
                 var _importStr = _importStrArr[_importStrI];
                 var _importPackage = _importStr.split(/['"]/)[1];
@@ -579,11 +579,11 @@ magnifyall.System = function(_settings){
                     _extendStr = _extendStr.substring(_extendStr.indexOf('{')+1,_extendStr.lastIndexOf('}'));
                     _fileString = _fileString.replace(_importStr, _extendStr);
                 }else{
-                    _isExtendClassIsImported = false;                    
+                    _isExtendClassIsImported = false;
                     _packageArr.push(_importPackage);
                 }
             }
-            
+
         }
         if(!_isExtendClassIsImported){
             if(!_classList[_importConfig.package])_importQueue.unshift({package: _importConfig.package});
@@ -596,7 +596,7 @@ magnifyall.System = function(_settings){
     }
 
     /*
-     * loop Import Process 
+     * loop Import Process
      */
     var _importProcess = function(){
         _isImporting = true;
@@ -606,7 +606,7 @@ magnifyall.System = function(_settings){
             _importLoopTrigger();
             return;
         }
-        while(_classList[_importConfig.package]){
+        while(_classList[_importConfig.package] && _importQueue.length>0){
             _importConfig = _importQueue.shift();
         }
         var _currentImportPackage = _importConfig.package;
@@ -619,7 +619,7 @@ magnifyall.System = function(_settings){
                 }
                 _fileString = _addDependedImport(_fileString, _importConfig);
                 _fileString = _initiatePackage(_currentImportPackage, _fileString);
-                if(_aopConfiguration){                    
+                if(_aopConfiguration){
                     _fileString = _initiateAspect(_fileString, _currentImportPackage);
                 }
                 _initiateProxy(_fileString, _instance.lib, _instance.src, _currentImportPackage, function(_fileString){
@@ -647,14 +647,14 @@ magnifyall.System = function(_settings){
                         }
                     }
                     _importLoopTrigger();
-                });                   
+                });
             }else{
                 _importLoopTrigger();
             }
         });
-        
+
     };
-    
+
     /*
     Execute System after decleration
     */
@@ -679,7 +679,7 @@ magnifyall.System = function(_settings){
             this.jsonLDDefination = {};
         }
         // if jsonLDProvieded load application
-        
+
         this.mainApp = {};
         this.mainAppElements = [];
         if(this.jsonLDDefination.push){
